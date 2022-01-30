791180125bd3d064f74f3063b3e60c0ff624fe07
olefirenko <dmitriy.olefirenko@gmail.com>
2022-01-30 13:45:30 +0800

    Initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import CameraScanner from "./components/CameraScanner.vue";
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>Vue Barcode Reader Demo</h1>
+  <CameraScanner />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CameraScanner.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from "vue";
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+const decodedText = ref("");
+
+const onLoaded = () => {
+  console.log("loaded");
+};
+
+const onDecode = (text) => {
+  decodedText.value = text;
+};
+</script>
+
+<template>
+  <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+  <h2>The decoded value in QR/barcode is</h2>
+  <h2>{{ decodedText }}</h2>
+
+  <p class="information">
+    <strong>Github:</strong>
+    <a href="https://github.com/olefirenko/vue-barcode-reader" target="_blank"> https://github.com/olefirenko/vue-barcode-reader</a>
+  </p>
+  <p>
+    <strong>NPM:</strong>
+    <a href="https://www.npmjs.com/package/vue-barcode-reader" target="_blank"> https://www.npmjs.com/package/vue-barcode-reader</a>
+  </p>
+  <p>
+    Check <strong>vue-barcode-reader</strong> library in production at
+    <a href="https://parceltrackingapp.com/en" target="_blank">ParcelTrackingApp.com</a>
+  </p>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+.information {
+  margin-top: 100px;
+}
+</style>
